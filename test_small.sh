50ce4ac6ce92554cbc5e38496d7781a196fd9efa
limiao2 <miaolior@gmail.com>
2023-05-21 21:40:06 +0000

    add multiple command for different tracking method

--- test_small.sh
+++ test_small.sh
@@ -1,1 +1,3 @@
-python3 val.py --tracking-method bytetrack --benchmark SNMOT --split test_small --processes-per-device 4
+python3 val.py --tracking-method bytetrack --benchmark SNMOT --split test_small --processes-per-device 4 --name yolov8_bytetrack_mobilenet --device 0
+
+python3 val.py --tracking-method deepocsort --benchmark SNMOT --split test_small --processes-per-device 4 --name yolov8_deepocsort_mobilenet --device 0
