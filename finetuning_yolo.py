cfdaaac9e337452c759fb403254bd62b89e50a3d
Shail Trivedi <33336214+shailtrivedi@users.noreply.github.com>
2023-05-21 19:53:37 -0700

    +finetuning results, weights too big to upload

--- finetuning_yolo.py
+++ finetuning_yolo.py
@@ -9,6 +9,6 @@ torch.cuda.empty_cache()
 model = YOLO("yolov8l.pt")  # load a pretrained model (recommended for training)
 
 # Use the model
-model.train(data="/home/ubuntu/cs231n/data/yolo_base/data.yaml", epochs=10, patience=3, batch=8, imgsz=1088, name='yolov8l_trial_1_ST', optimizer='Adam', lr0=1e-3, workers=4)  # train the model
+model.train(data="/home/ubuntu/cs231n/data/yolo_base/data.yaml", epochs=10, patience=3, imgsz=640, name='yolov8l_trial_1_ST_', optimizer='Adam', lr0=1e-3, pretrained=True, batch=24)  # train the model
 metrics = model.val()  # evaluate model performance on the validation set
 success = model.export(format='onnx', device=0)  # export the model to ONNX format
